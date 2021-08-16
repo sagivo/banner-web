7f0ce2c50a2603d3406a7d6c4e7c718aa8052ac4
Sagiv <sagiv4@gmail.com>
2021-08-16 10:33:33 -0600

    a

--- src/Billboard.js
+++ src/Billboard.js
@@ -29,6 +29,13 @@ function Billboard() {
 
   return (
     <div id="container">
+      <div class="sky">
+        <div class="moon">
+          <div class="stars stars1">*</div>
+          <div class="stars stars2">*</div>
+          <div class="shooting-star"></div>
+        </div>
+      </div>
       <div id="billboard" style={{ fontSize: `${fontSize}px` }}>
         {message}
       </div>
@@ -36,7 +43,7 @@ function Billboard() {
       <div id="lights">
         <div class="light"></div>
         <div class="light"></div>
-        <div class="light"></div>
+        <div class="light light1"></div>
         <div class="light"></div>
       </div>
 
